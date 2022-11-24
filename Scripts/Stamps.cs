a66a146ce334e1a3995d9eb703df9350acdefff8
Ragna022 <gideonite4christ2017@gmail.com>
2022-11-24 20:20:06 +0100

    s

---
+++ Scripts/Stamps.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stamps : MonoBehaviour
+{
+    public PickStampsManager psm;
+    public int x;
+    public int stampCount;
+ 
+   private int PickStampsManager = Stamps.PickStampsManager ++;
+
+
+   
+        }
+
